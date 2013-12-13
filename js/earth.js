dd75d21829fd271c7ee9aa2f29d775e0d014abdc
Iain van Gardingen <iainvg@gmail.com>
2013-12-13 20:18:07 +0000

    up to date files

---
+++ js/earth.js
@@ -0,0 +1,95 @@
+// Created by Iain van Gardingen, for DAT301 Project, SeismoTweet
+(function () {
+
+	var webglEl = document.getElementById('webgl');
+
+	if (!Detector.webgl) {
+		Detector.addGetWebGLMessage(webglEl);
+		return;
+	}
+
+	var width  = window.innerWidth,
+		height = window.innerHeight;
+
+	// Earth params
+	var radius   = 0.5,
+		segments = 32,
+		rotation = 6;  
+
+	var scene = new THREE.Scene();
+
+	var camera = new THREE.PerspectiveCamera(45, width / height, 0.01, 1000);
+	camera.position.z = 1.5;
+
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(width, height);
+
+	scene.add(new THREE.AmbientLight(0x333333));
+
+	var light = new THREE.DirectionalLight(0xffffff, 1);
+	light.position.set(5,3,5);
+	scene.add(light);
+
+    var sphere = createSphere(radius, segments);
+	sphere.rotation.y = rotation; 
+	scene.add(sphere)
+
+    var clouds = createClouds(radius, segments);
+	clouds.rotation.y = rotation;
+	scene.add(clouds)
+
+	var stars = createStars(90, 64);
+	scene.add(stars);
+
+	var controls = new THREE.TrackballControls(camera);
+
+	webglEl.appendChild(renderer.domElement);
+
+	render();
+
+	function render() {
+		controls.update();
+		sphere.rotation.y += 0.0005;
+		clouds.rotation.y += 0.0005;		
+		requestAnimationFrame(render);
+		renderer.render(scene, camera);
+	}
+/*Create the globe */
+	function createSphere(radius, segments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments),
+			new THREE.MeshPhongMaterial({
+				/*Add the base image for the world map*/
+				map:         THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
+				/*This then adds the texture map to make mountain ranges 3 dimensional */
+				bumpMap:     THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
+				bumpScale:   0.03,
+				/*Then we add a graphic to give the water some 'movement'*/
+				specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
+				specular:    new THREE.Color('grey')								
+			})
+		);
+	}
+
+/*Time to initialise the moving clouds*/
+	function createClouds(radius, segments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius + 0.003, segments, segments),			
+			new THREE.MeshPhongMaterial({
+				map:         THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
+				transparent: true
+			})
+		);		
+	}
+/*And finally we add the stars to create a more realistic viewpoint in relation to the world*/
+	function createStars(radius, segments) {
+		return new THREE.Mesh(
+			new THREE.SphereGeometry(radius, segments, segments), 
+			new THREE.MeshBasicMaterial({
+				map:  THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'), 
+				side: THREE.BackSide
+			})
+		);
+	}
+
+}());
